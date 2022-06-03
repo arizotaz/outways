e802fc4383291c4e82190e0ca492a7a6f571379f
Colton Staiduhar <arizotaz@Coltons-MacBook-Pro.local>
2022-06-02 21:38:15 -0400

    ...

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 document.body.innerHTML = "A new update is available<br><button onclick='installUpdate()'>Would you like to update now?</button>";
 function installUpdate() {
   window.parent.fs.removeDirectory("C:/outways/bin/boot");
-  window.parent.setTimeout(function(){command('run /outways/run')},1000);
-  window.parent.closeOutways();
+  window.parent.fs.removeDirectory("C:/outways/setup");
+  window.parent.command('run /outways/run');
 }
