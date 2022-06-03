5fede136359fd03a2f0f7582464a32136f49ee6d
Colton Staiduhar <arizotaz@Coltons-MacBook-Pro.local>
2022-06-02 21:58:23 -0400

    ...

--- bin/programs/power.js
+++ bin/programs/power.js
@@ -1,15 +1,1 @@
-document.body.innerHTML = "<style>button {width:200px;}</style><div align=center><button onclick='close();'>Close Outways</button><button onclick='restart()'>Restart Outways</button><br><br><button onclick='reboot()'>Restart System</button><button onclick='exit()'>Exit System</button></div>";
-function close() {
-  window.parent.closeOutways();
-}
-function restart() {
-  window.parent.setTimeout(function(){command("run /outways/run");},500);
-  window.parent.closeOutways();
-}
-function reboot() {
-  window.parent.chrome.runtime.reload();
-}
-function exit() {
-  window.parent.closeOutways();
-  window.parent.command("exit");
-}
+document.body.innerHTML = "<style>button {width:200px;}</style><div align=center><button onclick='window.parent.closeOutways();'>Close Outways</button><button onclick='window.parent.command('run /outways/run')'>Restart Outways</button><br><br><button onclick='window.parent.chrome.runtime.reload();'>Restart System</button><button onclick='window.parent.command('exit');'>Exit System</button></div>";
